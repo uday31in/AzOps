6641f3a0796af06195a310d0638ad1b6173c01d5
uday31in <14359777+uday31in@users.noreply.github.com>
2021-01-26 23:24:44 +0000

    deployment

--- AzOps/data/template/aks.sh
+++ AzOps/data/template/aks.sh
@@ -1,29 +1,44 @@
 #!/bin/bash
 
-if [ -z "$AKS_CLUSTER_NAME" ]; then
-    echo "must provide AKS_CLUSTER_NAME env var"
-    exit 1;
-fi
+# if [ -z "$AKS_CLUSTER_NAME" ]; then
+#     echo "must provide AKS_CLUSTER_NAME env var"
+#     exit 1;
+# fi
 
-if [ -z "$AKS_CLUSTER_RESOURCE_GROUP_NAME" ]; then
-    echo "must provide AKS_CLUSTER_RESOURCE_GROUP_NAME env var"
-    exit 1;
-fi
+# if [ -z "$AKS_CLUSTER_RESOURCE_GROUP_NAME" ]; then
+#     echo "must provide AKS_CLUSTER_RESOURCE_GROUP_NAME env var"
+#     exit 1;
+# fi
 
-#download kubctl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+function configureKubectl()
+{
+    #download kubctl
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 
-#move kubectl  to local path
-mkdir -p ~/.local/bin/kubectl
-mv ./kubectl ~/.local/bin/kubectl
-chmod +x ~/.local/bin/kubectl/kubectl
-PATH=$PATH:~/.local/bin/kubectl
+    #move kubectl  to local path
+    mkdir -p ~/.local/bin/kubectl
+    mv ./kubectl ~/.local/bin/kubectl
+    chmod +x ~/.local/bin/kubectl/kubectl
+    PATH=$PATH:~/.local/bin/kubectl
 
-#get AKS credentials
-az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $AKS_CLUSTER_RESOURCE_GROUP_NAME --admin
+    #get AKS credentials
+    az aks get-credentials --name $AKS_CLUSTER_NAME --resource-group $AKS_CLUSTER_RESOURCE_GROUP_NAME --admin
 
-#cat /root/.kube/config;
+    cat /root/.kube/config;
+}
 
-echo \"Running Command: $1\"
-result=$($1)
-echo $result>$AZ_SCRIPTS_OUTPUT_PATH
+function runKubectlCommand()
+{
+    echo "number of arguments $#"
+    echo "Running Command: $*"
+
+    read -r -d '' AZ_SCRIPTS_OUTPUT_PATH <<< $($*)
+    echo "result: $AZ_SCRIPTS_OUTPUT_PATH"
+}
+
+
+main() {
+    configureKubectl
+    runKubectlCommand "$@"
+}
+main "$@"
